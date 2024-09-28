03fb41c84de27ed0ac1867df1ac2a6980cd04d21
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2024-09-28 16:55:40 +0300

    Adding BaseClient, using as super
    Использование класса BaseClient как класс, из которого наследуются свойства

--- ClassesModel/Client.py
+++ ClassesModel/Client.py
@@ -1,16 +1,13 @@
 import re
 import json
+from ClassesModel.BaseClient import BaseClient
 
-class Client:
-    def __init__(self, client_id, fullname, phone_number, male, email, age, allergic_reactions, document):
-        self.__client_id = self.validate(client_id, self.validate_client_id)
-        self.__fullname = self.validate(fullname, self.validate_fullname)
-        self.__phone_number = self.validate(phone_number, self.validate_phone_number)
-        self.__male = self.validate(male, self.validate_male)
-        self.__email = self.validate(email, self.validate_email)
-        self.__age = self.validate(age, self.validate_age)
-        self.__allergic_reactions = allergic_reactions 
-        self.__document = document
+class Client(BaseClient):
+    def __init__(self, fullname, phone_number, male, email, age, allergic_reactions, document):
+        super(Client, self).__init__(__fullname=__fullname, __phone_number=__phone_number, __email=__email, __document=__document)
+        self.set_male(male)
+        self.set_age(age)
+        self.set_allergic_reactions(allergic_reactions)
 
     # Классовый метод создания клиента из JSON
     @classmethod
@@ -18,7 +15,6 @@ class Client:
         try:
             data = json.loads(data_json)
             return Client(
-                client_id=data['client_id'],
                 fullname=data['fullname'],
                 phone_number=data['phone_number'],
                 male=data['male'],
@@ -37,28 +33,10 @@ class Client:
 
     # Статические методы валидации
     @staticmethod
-    def validate_fullname(fullname):
-        if not isinstance(fullname, str) or len(fullname) == 0:
-            raise ValueError("ФИО введено неверно (не может быть пустым значением).")
-        return fullname
-
-    @staticmethod
-    def validate_phone_number(phone_number):
-         if not isinstance(phone_number, str) or not re.fullmatch(r'((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}', phone_number):
-            raise ValueError('Номер телефона введен неверно.')
-        return phone_number
-
-    @staticmethod
     def validate_male(male):
         if male not in ('М', 'Ж'):
             raise ValueError("Пол должен быть 'М' или 'Ж'.")
         return male
-
-    @staticmethod
-    def validate_email(email):
-        if not isinstance(email, str) or not re.fullmatch(r'(.+)@(.+)\.(.+)', email):
-            raise ValueError("Электронная поста введена неверно.")
-        return email
 
     @staticmethod
     def validate_age(age):
@@ -67,9 +45,9 @@ class Client:
         return age
 
     @staticmethod
-    def validate_document(document):
-        if not isinstance(document, str) or not re.fullmatch(r'\d{4} \d{6}', document):
-            raise ValueError('Невреные данные паспорта (документа).')
+    def validate_allergic_reactions(document):
+        if not isinstance(allergic_reactions, str):
+            raise ValueError('Аллергические реакции должны быть введены строкой.')
         return document
 
    # Вывод полной версии объекта 
@@ -105,20 +83,8 @@ class Client:
         return False
 
     # Getters
-    def get_client_id(self):
-        return self.__client_id
-
-    def get_fullname(self):
-        return self.__fullname
-
-    def get_phone_number(self):
-        return self.__phone_number
-
     def get_male(self):
         return self.__male
-
-    def get_email(self):
-        return self.__email
 
     def get_age(self):
         return self.__age
@@ -126,27 +92,15 @@ class Client:
     def get_allergic_reactions(self):
         return self.__allergic_reactions
 
-    def get_document(self):
-        return self.__document
-
     # Setters
-    def set_fullname(self, fullname):
-        self.__fullname = self.validate(fullname, self.validate_fullname)
-
-    def set_phone_number(self, phone_number):
-        self.__phone_number = self.validate(phone_number, self.validate_phone_number)
-
     def set_male(self, male):
         self.__male = self.validate(male, self.validate_male)
-
-    def set_email(self, email):
-        self.__email = self.validate(email, self.validate_email)
 
     def set_age(self, age):
         self.__age = self.validate(age, self.validate_age)
 
     def set_allergic_reactions(self, allergic_reactions):
-        self.__allergic_reactions = allergic_reactions
+        self.__allergic_reactions = self.validate(allergic_reactions, self.validate_allergic_reactions)
 
     def set_document(self, document):
         self.__document = self.validate(document, self.validate_document)
