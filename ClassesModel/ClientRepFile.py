ce39a95f028ff001bf8f1aea6301390f5011a06c
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2024-11-17 18:24:47 +0300

    Lab. 2.6 Update class ClientRepFile
    Изменение структуры класса, для использования адаптера

--- ClassesModel/ClientRepFile.py
+++ ClassesModel/ClientRepFile.py
@@ -4,19 +4,19 @@ from ClientRepFileStrategy import ClientRepFileStrategy
 # Класс, который использует стратегию для работы с файлами
 class ClientRepFile:
     def __init__(self, strategy: ClientRepFileStrategy):
-        self.strategy = strategy
+        self._data = []
+        self._strategy = strategy
 
-    def get_all(self):
-        """Получить все элементы"""
-        return self.strategy.read()
+    def write_data_to_file(self):
+        self._strategy.write(self._data)
+
+    def read_data_from_file(self):
+        self._data = self._strategy.read()
 
     def add_entity(self, fullname, phone_number, male, email, age, allergic_reactions, document):
         """Добавить новый объект в список с новым ID"""
-        # Чтение данных из файла
-        data = self.strategy.read()
-
         # Генерация нового ID
-        new_id = max([entry['id'] for entry in data], default=0) + 1
+        new_id = max([entry['id'] for entry in self._data], default=0) + 1
         new_entity = {
             'id': new_id,
             'fullname': fullname,
@@ -29,46 +29,39 @@ class ClientRepFile:
         }
 
         # Проверка на уникальность документа
-        if any(entry['document'] == document for entry in data):
+        if any(entry['document'] == document for entry in self._data):
             raise ValueError('Паспорт должен быть уникальным!')
 
         # Добавляем новый объект в список
-        data.append(new_entity)
-
-        # Записываем обновленные данные в файл
-        self.strategy.write(data)
+        self._data.append(new_entity)
 
     def get_by_id(self, id):
         """Получить объект по ID"""
-        data = self.strategy.read()
-        for entry in data:
+        for entry in self._data:
             if entry['id'] == id:
                 return entry
         return None  # Если объект не найден
 
     def get_k_n_short_list(self, k, n):
         """Получить список k по счету n объектов"""
-        data = self.strategy.read()
         start = (n - 1) * k
         end = start + k
-        return data[start:end]
+        return self._data[start:end]
 
     def sort_by_field(self, field):
         """Сортировать элементы по выбранному полю"""
-        data = self.strategy.read()
         if field in ["fullname", "email", "age"]:
-            data.sort(key=lambda x: x.get(field))
-        return data
+            self._data.sort(key=lambda x: x.get(field))
+        return self._data
 
     def replace_by_id(self, entity_id, fullname, phone_number, male, email, age, allergic_reactions, document):
         """Заменить элемент списка по ID"""
-        data = self.strategy.read()
         entity = self.get_by_id(entity_id)
         if not entity:
             raise ValueError(f"Элемент с ID {entity_id} не найден.")
 
         # Проверка на уникальность паспорта
-        if document and document != entity['document'] and any(entry['document'] == document for entry in data):
+        if document and document != entity['document'] and any(entry['document'] == document for entry in self._data):
             raise ValueError('Паспорт должен быть уникальным!')
 
         # Обновляем данные
@@ -87,19 +80,17 @@ class ClientRepFile:
             if document:
                 entity['document'] = document
 
-            # Записываем обновленные данные в файл
-            self.strategy.write(data)
-
     def delete_by_id(self, entity_id):
             """Удалить элемент списка по ID"""
-            data = self.strategy.read()
             entity = self.get_by_id(entity_id)
             if not entity:
                 raise ValueError(f"Элемент с ID {entity_id} не найден.")
 
-            data.remove(entity)
-            self.strategy.write(data)
+            self._data.remove(entity)
 
+    def get_count(self):
+            """Получить количество элементов"""
+            return len(self._data)
     def get_count(self):
             """Получить количество элементов"""
             data = self.strategy.read()
