976393af52e749260f0edeeb8ee40813d40d5efc
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2024-11-17 13:13:52 +0300

    Lab. 1.9 adding id
    Добавление id

--- ClassesModel/BaseClient.py
+++ ClassesModel/BaseClient.py
@@ -2,7 +2,8 @@ import re
 
 
 class BaseClient:
-    def __init__(self, fullname, phone_number, email, document):
+    def __init__(self, id = None, fullname, phone_number, email, document):
+        self.__id = id
         self.set_fullname(fullname)
         self.set_phone_number(phone_number)
         self.set_email(email)
