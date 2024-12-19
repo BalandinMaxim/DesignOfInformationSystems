7c7034a497ded220d36457f1724c5a7d970a237b
BalandinMaxim <112868629+BalandinMaxim@users.noreply.github.com>
2024-12-19 20:01:56 +0300

    Lab. 6 Changing assosiatin between database and rep_file classes
    Изменение типа ассоциации между подулючение к бд и раюоты с бд

--- ClassesModel/Main.py
+++ ClassesModel/Main.py
@@ -1,7 +1,10 @@
 from Client_rep_DB import ClientRepDB
-from ClientModel import ClientModel
+from FasadModel import FasadModel
 from MainView import MainView
 from MainController import MainController
+from DatabaseConnection import DatabaseConnection
+from Doctor_rep_DB import DoctorRepDB
+from ServiseLog_rep_DB import ServiceLogRepDB
 import tkinter as tk
 
 if __name__ == "__main__":
@@ -15,11 +18,16 @@ if __name__ == "__main__":
     }
 
     # Создание репозитория и модели
-    repository = ClientRepDB(db_config)
-    model = ClientModel(repository)
+    db_connection = DatabaseConnection(db_config)
+    repositoryClient = ClientRepDB(db_connection)
+    repositoryDoctor = DoctorRepDB(db_connection)
+    repositoryService = ServiceLogRepDB(db_connection)
+
+    model = FasadModel(repositoryClient, repositoryDoctor, repositoryService)
 
     # Создание и запуск главного окна
     root = tk.Tk()
     main_controller = MainController(model)
     MainView(root, main_controller)
     root.mainloop()
+
